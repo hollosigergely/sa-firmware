75cd48a70a3416febd581d0ffbb0b8468ac6ad6b
Hollósi Gergely <hollosigergely@gmail.com>
2020-03-31 09:59:02 +0200

    add: address handling mechnism based on UICR

--- firmware/address_handler.c
+++ firmware/address_handler.c
@@ -6,32 +6,20 @@
 
 #define TAG "addr"
 
-typedef struct  {
-    uint64_t    mac_addr;
-    uint16_t    virt_addr;
-    bool        is_anchor;
-} device_addr_t;
-
-
-static device_addr_t m_device_db[] = {
-// Tags
-    { 0xD90731EA504E, 0x0000, false },
-
-// Anchors
-    { 0xFDE6286BE9AB, 0x0000, true },
-    { 0x5C676BE0E853, 0x0001, true },
-    { 0xCF458A24912A, 0x0002, true },
-    { 0x4FE21045C08E, 0x0003, true },
-    { 0x0DF4EC56D725, 0x0004, true },
-    { 0x61DB9F4C6157, 0x0005, true },
-
-};
-#define DEVICE_DB_LENGTH    (sizeof(m_device_db)/sizeof(m_device_db[0]))
+typedef struct
+{
+    uint16_t    group_id;
+    uint16_t    device_id:15;
+    uint8_t     is_anchor:1;
+} __attribute__((packed)) uicr_data_t;
+volatile uicr_data_t m_uicr_data  __attribute__ ((section(".uicr_customer")));
 
 static uint64_t m_mac_address;
 static uint16_t m_virtual_address;
+static uint16_t m_group_id;
 static bool     m_is_anchor;
 static char     m_device_name[26];
+
 
 void addr_handler_init() {
     m_mac_address = *((uint64_t*)&NRF_FICR->DEVICEADDR[0]);
@@ -39,30 +27,28 @@ void addr_handler_init() {
     m_mac_address |= 0x0000c00000000000;
 
     LOGI(TAG,"MAC addr: 0x%" PRIX64 "\n", m_mac_address);
+    LOGT(TAG, "UICR: 0x%08X\n", *((uint32_t*)&m_uicr_data));
 
-    for(size_t i = 0; i < DEVICE_DB_LENGTH; i++) {
-        if((m_device_db[i].mac_addr | 0x0000c00000000000) == m_mac_address)
-        {
-            m_virtual_address = m_device_db[i].virt_addr;
-            m_is_anchor = m_device_db[i].is_anchor;
+    m_virtual_address = m_uicr_data.device_id;
+    m_is_anchor = m_uicr_data.is_anchor;
+    m_group_id = m_uicr_data.group_id;
 
-            if(m_is_anchor)
-            {
-                sprintf(m_device_name, "Anchor 0x%04X", addr_handler_get_virtual_addr());
-            }
-            else
-            {
-                sprintf(m_device_name, "Tag 0x%04X", addr_handler_get_virtual_addr());
-            }
-
-            LOGI(TAG, "device: %s\n", m_device_name);
-            LOGI(TAG, "group: 0x%04X\n", addr_handler_get_group_id());
-
-            return;
-        }
+    if(m_is_anchor)
+    {
+        sprintf(m_device_name, "Anchor 0x%04X", addr_handler_get_virtual_addr());
+    }
+    else
+    {
+        sprintf(m_device_name, "Tag 0x%04X", addr_handler_get_virtual_addr());
     }
 
-    ERROR(TAG,"No address specified.\n");
+    LOGI(TAG, "device: %s\n", m_device_name);
+    LOGI(TAG, "group: 0x%04X\n", addr_handler_get_group_id());
+
+    if(m_uicr_data.group_id == 0)
+    {
+        ERROR(TAG,"No address specified (group_id == 0).\n");
+    }
 }
 
 uint64_t addr_handler_get_mac_addr() {
@@ -86,7 +72,7 @@ char *addr_handler_get_device_name()
 
 uint16_t addr_handler_get_group_id()
 {
-    return 0xBABA;
+    return m_group_id;
 }
 
 
